9c909f4aeac50994802a5ab889bfe6fdacab77d6
Justin Cook <jcook03266@gmail.com>
2023-02-13 11:44:18 -0500

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Justin Cook on 2/13/23.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

b54337faa0ffa107696a7f763ffa71bef5fc1d8b
Justin Cook <jcook03266@gmail.com>
2023-02-18 23:34:06 -0500

    Delete PokedexGraphQL_CodeGenConfigInitScript.sh

--- PokedexGraphQL_CodeGenConfigInitScript.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Initialize the code generation configuration JSON
-./apollo-ios-cli init --schema-name PokemonSchema --module-type embeddedInTarget --target-name Pokedex
-
-# Generate the required schema and GraphQL swift code
-./apollo-ios-cli generate}
